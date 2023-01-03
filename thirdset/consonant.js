b823f0ac42c6eaaa2c902ae4b6d3b04a1996b052
Mazhar iqbal siddiquee <mazhariqbal963169@gmail.com>
2023-01-03 18:28:48 +0530

    2nd commit

---
+++ thirdset/consonant.js
@@ -0,0 +1,9 @@
+let char ="a"
+if(!char=="a"&&!char=="e"&&!char=="i"&&!char=="o"&&!char=="u")
+{
+  console.log("consonant");
+}
+else
+{
+  console.log("vowel");
+}
---
+++ thirdset/threeno.js
@@ -0,0 +1,15 @@
+let a=10;
+let b=6;
+let c=15;
+if(a>b&& a>c)
+{
+  console.log("a is greater");
+}
+else if(b>a&& b>c)
+{
+  console.log("b is greater");
+}
+else 
+{
+  console.log("c is greater");
+}
